e32b6e1a9c7cf28afaa182da1d2bcddd95f8924b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PrintingMultipleBarCharts/XtraReport1.cs
@@ -0,0 +1,15 @@
+using System;
+using DevExpress.XtraReports.UI;
+// ...
+
+namespace PrintingMultipleBarCharts
+{
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public XtraReport1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
